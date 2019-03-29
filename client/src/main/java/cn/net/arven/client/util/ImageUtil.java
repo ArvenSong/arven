971b3895b1483595e6d146fc9d2359378e8f034a
songjianhua_o <songjianhua@365trade.com.cn>
2019-03-29 14:25:01 +0800

    多图片转pdf

--- client/src/main/java/cn/net/arven/client/util/ImageUtil.java
+++ client/src/main/java/cn/net/arven/client/util/ImageUtil.java
@@ -8,8 +8,21 @@ import ij.process.ImageProcessor;
 import java.awt.*;
 import java.io.File;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfWriter;
 
 /**
  * 图片工具类
@@ -19,18 +32,30 @@ import java.util.UUID;
  **/
 public class ImageUtil {
 
-    public static void product() {
+    public static void main(String[] args) {
         String desktop = "C:\\";
         String a4blank = "picture\\A4blank.jpg";
         String a4blankLabel = "picture\\labelBlank.jpg";
         String source = "picture\\source";
 
         String finished = "picture\\finished\\";
-        Opener opener = new Opener();
-        ImagePlus blankImp = opener.openImage(desktop + a4blank);
-        ImagePlus blankLabelImp = opener.openImage(desktop + a4blankLabel);
+        produce(desktop, a4blank, a4blankLabel, source, finished);
+    }
 
-        File folder = new File(desktop + source);
+    /**
+     * 运行
+     * @param rootFolder
+     * @param a4blank
+     * @param a4blankLabel
+     * @param source
+     * @param finished
+     */
+    private static void produce(String rootFolder, String a4blank, String a4blankLabel, String source, String finished) {
+        Opener opener = new Opener();
+        ImagePlus blankImp = opener.openImage(rootFolder + a4blank);
+        ImagePlus blankLabelImp = opener.openImage(rootFolder + a4blankLabel);
+
+        File folder = new File(rootFolder + source);
         List<File> tempList = Arrays.asList(folder.listFiles());
 
         for (File file : tempList) {
@@ -39,16 +64,17 @@ public class ImageUtil {
             joinPicture(blankImp, oneImp, tempList.indexOf(file)%4, name);
             joinLabel(blankLabelImp, tempList.indexOf(file) % 12, name);
             if (tempList.indexOf(file)%4==3||tempList.indexOf(file)==tempList.size()-1){
-                IJ.saveAs(blankImp, "jpeg", desktop+finished+UUID.randomUUID());
+                IJ.saveAs(blankImp, "jpeg", rootFolder +finished+UUID.randomUUID());
                 blankImp.revert();
             }
             if (tempList.indexOf(file) % 12 == 11 || tempList.indexOf(file) == tempList.size() - 1) {
-                IJ.saveAs(blankLabelImp, "jpeg", desktop + finished + UUID.randomUUID());
+                IJ.saveAs(blankLabelImp, "jpeg", rootFolder + finished + UUID.randomUUID());
 //                blankLabelImp.show();
                 blankLabelImp.revert();
             }
 
         }
+        toPdf(rootFolder + finished, rootFolder + finished+new Date().getTime()+".pdf");
     }
 
     /**
@@ -332,5 +358,70 @@ public class ImageUtil {
             loc[7] = 20;
         }
     }
+
+
+    /**
+     *
+     * @param imageFolderPath
+     *            图片文件夹地址
+     * @param pdfPath
+     *            PDF文件保存地址
+     *
+     */
+    public static void toPdf(String imageFolderPath, String pdfPath) {
+        try {
+            // 图片文件夹地址
+            // String imageFolderPath = "D:/Demo/ceshi/";
+            // 图片地址
+            String imagePath = null;
+            // PDF文件保存地址
+            // String pdfPath = "D:/Demo/ceshi/hebing.pdf";
+            // 输入流
+            FileOutputStream fos = new FileOutputStream(pdfPath);
+            // 创建文档
+            Document doc = new Document(null, 0, 0, 0, 0);
+            //doc.open();
+            // 写入PDF文档
+            PdfWriter.getInstance(doc, fos);
+            // 读取图片流
+            BufferedImage img = null;
+            // 实例化图片
+            Image image = null;
+            // 获取图片文件夹对象
+            File file = new File(imageFolderPath);
+            File[] files = file.listFiles();
+            // 循环获取图片文件夹内的图片
+            for (File file1 : files) {
+                if (file1.getName().endsWith(".png")
+                        || file1.getName().endsWith(".jpg")
+                        || file1.getName().endsWith(".gif")
+                        || file1.getName().endsWith(".jpeg")
+                        || file1.getName().endsWith(".tif")) {
+                    // System.out.println(file1.getName());
+                    imagePath = imageFolderPath + file1.getName();
+//                    System.out.println(file1.getName());
+                    // 读取图片流
+                    img = ImageIO.read(new File(imagePath));
+                    // 根据图片大小设置文档大小
+                    doc.setPageSize(new Rectangle(img.getWidth(), img
+                            .getHeight()));
+                    // 实例化图片
+                    image = Image.getInstance(imagePath);
+                    // 添加图片到文档
+                    doc.open();
+                    doc.add(image);
+                }
+            }
+            // 关闭文档
+            doc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BadElementException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
