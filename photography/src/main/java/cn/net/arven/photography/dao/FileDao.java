af53b0c4504ce3b7d01ffbbcbc679455feac484f
songjianhua_o <songjianhua@365trade.com.cn>
2019-04-29 14:13:27 +0800

    新增模块photography

---
+++ photography/src/main/java/cn/net/arven/photography/dao/FileDao.java
@@ -0,0 +1,16 @@
+package cn.net.arven.photography.dao;
+
+import cn.net.arven.common.entity.File;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2018-12-22
+ */
+public interface FileDao extends BaseMapper<File> {
+
+}
